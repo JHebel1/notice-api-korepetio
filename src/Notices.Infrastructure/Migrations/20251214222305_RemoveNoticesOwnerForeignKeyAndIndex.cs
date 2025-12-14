494be7d68a358b63d3a243df51d29fd541c17c41
Konrad Cichosz <konrad.cichosz@talentech.com>
2025-12-14 23:23:57 +0100

    chore: drop Notices.OwnerId foreign key and index

---
+++ src/Notices.Infrastructure/Migrations/20251214222305_RemoveNoticesOwnerForeignKeyAndIndex.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Notices.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveNoticesOwnerForeignKeyAndIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notices_Users_OwnerId",
+                table: "Notices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Notices_OwnerId",
+                table: "Notices");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Notices_OwnerId",
+                table: "Notices",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notices_Users_OwnerId",
+                table: "Notices",
+                column: "OwnerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
